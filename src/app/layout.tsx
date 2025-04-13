7460276635fc11427b30048b7e3c34814f89117b
지오현 <jihkyuoo@naver.com>
2025-04-14 01:06:04 +0900

    prettier format 실행

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Header } from '@/components/layout/header/Header';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "성복교회",
-  description: "성복교회 홈페이지",
+  title: '성복교회',
+  description: '성복교회 홈페이지',
 };
 
 export default function RootLayout({
@@ -24,9 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import bgImgMain01 from '@/assets/images/main/main01.jpg';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Button>Click me</Button>
+      <Image src={bgImgMain01} alt="main01" fill />
     </>
   );
 }

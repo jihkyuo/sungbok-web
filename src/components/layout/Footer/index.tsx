8bf0d7db10358817ddf79b8463ea3f076da717ff
지오현 <jihkyuoo@naver.com>
2025-05-03 23:07:30 +0900

    feat: footer 추가

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,7 +1,52 @@
+interface Info {
+  [key: string]: {
+    label: string;
+    value: string;
+  };
+}
+
+const info: Info = {
+  chief: {
+    label: '담임목사',
+    value: '이요셉',
+  },
+  phone: {
+    label: 'Tel',
+    value: '02-2245-5840',
+  },
+  email: {
+    label: 'E-mail',
+    value: 'sungbok1979@naver.com',
+  },
+  address: {
+    label: '주소',
+    value: '서울시 동대문구 장안벚꽃로 139',
+  },
+};
+
 export const Footer = () => {
   return (
-    <footer className="flex items-end justify-center py-10 text-white bg-black h-100">
-      <p className="text-sm">Copyright 성복교회 All rights reserved.</p>
+    <footer className="flex h-100 flex-col items-center justify-between bg-black px-10 pt-30 pb-4 font-bold whitespace-nowrap text-white gap-10">
+      <div className={'flex flex-col items-center'}>
+        <h3 className={'mb-10 text-5xl'}>성복교회</h3>
+
+        <ul className={'flex flex-wrap justify-center gap-x-10 gap-y-4'}>
+          {Object.entries(info).map(([key, row]) => (
+            <li className={'flex gap-2 text-lg'} key={key}>
+              <span>{row.label}</span>
+              <span className={'opacity-50'}>{row.value}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={'flex flex-col items-center gap-2'}>
+        <div className={'text-lg'}>개인정보처리방침</div>
+
+        <p className="text-sm opacity-50">
+          Copyright <span className={'text-lg'}>성복교회</span> All Rights Reserved.
+        </p>
+      </div>
     </footer>
   );
 };

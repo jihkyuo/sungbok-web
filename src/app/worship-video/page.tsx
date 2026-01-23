b8a231915e4f0012262ad9a66283fb24e763cdc1
지오현 <jihkyuoo@naver.com>
2026-01-24 02:11:38 +0900

    fix: 예배 페이지 버그픽스

--- src/app/worship-video/page.tsx
+++ src/app/worship-video/page.tsx
@@ -1,16 +1,10 @@
-import { lazy, Suspense } from 'react';
-
-const PlaylistPagination = lazy(
-  () => import('@/domains/worship-video/components/widgets/PlaylistPagination')
-);
+import PlaylistPagination from '@/domains/worship-video/components/widgets/PlaylistPagination';
 
 export default function WorshipVideoPage() {
   return (
     <div>
       <div className={'h-60'} />
-      <Suspense fallback={<div>Loading...</div>}>
-        <PlaylistPagination />
-      </Suspense>
+      <PlaylistPagination />
     </div>
   );
 }
